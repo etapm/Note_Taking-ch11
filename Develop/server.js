c1508180449666e2ed8b296537f8ff3220d8a8ef
etapm <etap@ucdavis.edu>
2023-03-21 12:22:51 -0700

    Initial commit

--- Develop/server.js
+++ Develop/server.js
@@ -15,6 +15,26 @@ app.use(express.static("public"));
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
+// Bonus: DELETE Route
+app.delete("/api/notes/:id", (req, res) => {
+  const noteId = req.params.id;
+
+  fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    const updatedNotes = notes.filter((note) => note.id !== noteId);
+
+    fs.writeFile(
+      path.join(__dirname, "./db/db.json"),
+      JSON.stringify(updatedNotes),
+      (err) => {
+        if (err) throw err;
+        res.json({ success: true });
+      }
+    );
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
