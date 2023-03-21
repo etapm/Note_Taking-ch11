0a8d169c51a00875b66e19cc70fe48d07aa927d1
etapm <etap@ucdavis.edu>
2023-03-21 11:54:40 -0700

    saving notes updated in apiRoutes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,12 +1,34 @@
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 const router = express.Router();
 
 router.get("/notes", (req, res) => {
   fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
     if (err) throw err;
     res.json(JSON.parse(data));
+  });
+});
+
+// POST route for saving notes
+router.post("/notes", (req, res) => {
+  const newNote = req.body;
+  newNote.id = uuidv4();
+
+  fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    notes.push(newNote);
+
+    fs.writeFile(
+      path.join(__dirname, "../db/db.json"),
+      JSON.stringify(notes),
+      (err) => {
+        if (err) throw err;
+        res.json(newNote);
+      }
+    );
   });
 });
 
