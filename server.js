aaccf340e211df1c787637e3a432fef9f3647f33
etapm <etap@ucdavis.edu>
2023-03-21 18:25:01 -0700

    heroku server.js eddit to serve static files from public folder

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("./Develop/public"));
+app.use(express.static("Develop/public"));
 
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
